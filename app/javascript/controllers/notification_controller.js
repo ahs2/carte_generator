490ce091e5339e72a16203e833a9956bd9f7105a
ahs2 <adamou.dev@gmail.com>
2023-10-18 14:19:04 +0100

    deuxieme commit

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -3,6 +3,6 @@ import Notification from 'stimulus-notification'
 export default class extends Notification {
   connect() {
     super.connect()
-    console.log('Do what you want here.')
+    // console.log('Do what you want here.')
   }
 }
